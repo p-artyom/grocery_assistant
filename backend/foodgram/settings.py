be4ac380ceb2b44f814ed5e6c8fbacdc2635835b
Pilipenko Artem <121739130+p-artyom@users.noreply.github.com>
2023-08-26 08:46:13 +0800

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,9 +72,9 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'NAME': os.getenv('POSTGRES_DB', 'django_db'),
+        'USER': os.getenv('POSTGRES_USER', 'django_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django_password'),
         'HOST': os.getenv('DB_NAME', ''),
         'PORT': os.getenv('DB_PORT', 5432),
     },
